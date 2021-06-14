0323211e28aab608aea88628a346b1ac9a60bfa5
saya <wuyezhishang@163.com>
2021-06-15 02:05:38 +0800

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {getConnectionOptions} from "typeorm";
 
 @Module({
-  imports: []
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => Object.assign(await getConnectionOptions(), { autoLoadEntities: true })
+    })
+  ]
 })
 export class AppModule {}
